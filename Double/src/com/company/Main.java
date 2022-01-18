66cde7936a3cf54a6c9d6e68adb6cd05c12b79af
grmnvv <russianfracture@gmail.com>
2022-01-18 23:15:15 +0300

    first commit

---
+++ Double/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double a = 3;
+
+        double b = 4;
+
+
+        double c = Math.sqrt(a * a + b * b);
+
+        System.out.println ("c = "+ c);
+    }
+}
